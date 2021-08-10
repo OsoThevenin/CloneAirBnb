5235e15f438e1b57ed968b54c3636e71c7599d74
Pere Bruy Vilalta <pere.bruy@itequia.com>
2021-08-10 21:40:01 +0200

    Add range to search

--- pages/admin/homes.vue
+++ pages/admin/homes.vue
@@ -59,16 +59,16 @@
 				v-for="(range, index) in home.availabilityRanges"
 				:key="index"
 				v-model="home.availabilityRanges[index]"
-        is-range
-        timezone="UTC"
-        :modelConfig="{timeAdjust: '00:00:00'}"
+				is-range
+				timezone="UTC"
+				:modelConfig="{ timeAdjust: '00:00:00' }"
 			>
-      <template v-slot="{inputValue, inputEvents}">
-        <input :value="inputValue.start" v-on="inputEvents.start" />
-        to
-        <input :value="inputValue.end" v-on="inputEvents.end" /><br/>
-      </template>
-      </date-picker>
+				<template v-slot="{ inputValue, inputEvents }">
+					<input :value="inputValue.start" v-on="inputEvents.start" />
+					to
+					<input :value="inputValue.end" v-on="inputEvents.end" /><br />
+				</template>
+			</date-picker>
 			<button class="border px-4 py-2 border-gray-400">Add</button>
 		</form>
 	</div>
--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -1,113 +1,123 @@
 import { unWrap, getErrorResponse } from '~/utils/fetchUtils'
 
-export default function({$config}, inject) {
-  const headers = {
-    "X-Algolia-API-Key": $config.algolia.API_KEY,
-    "X-Algolia-Application-Id": $config.algolia.APPLICATION_ID,
-  };
+export default function ({ $config }, inject) {
+	const headers = {
+		'X-Algolia-API-Key': $config.algolia.API_KEY,
+		'X-Algolia-Application-Id': $config.algolia.APPLICATION_ID,
+	}
 
-  inject("dataApi", {
-    getHome,
-    getReviewsByHomeId,
-    getUserByHomeId,
-    getHomesByLocation,
-    getHomes,
-  });
+	inject('dataApi', {
+		getHome,
+		getReviewsByHomeId,
+		getUserByHomeId,
+		getHomesByLocation,
+		getHomes,
+	})
 
-  async function getHome(homeId) {
-    try {
-      return unWrap(
-        await fetch(
-          `https://${$config.algolia.APPLICATION_ID}-dsn.algolia.net/1/indexes/nuxtbnb_homes/${homeId}`,
-          { headers }
-        )
-      );
-    } catch (error) {
-      return getErrorResponse(error);
-    }
-  }
+	async function getHome(homeId) {
+		try {
+			return unWrap(
+				await fetch(
+					`https://${$config.algolia.APPLICATION_ID}-dsn.algolia.net/1/indexes/nuxtbnb_homes/${homeId}`,
+					{ headers }
+				)
+			)
+		} catch (error) {
+			return getErrorResponse(error)
+		}
+	}
 
-  async function getReviewsByHomeId(homeId) {
-    try {
-      return unWrap(
-        await fetch(
-          `https://${$config.algolia.APPLICATION_ID}-dsn.algolia.net/1/indexes/nuxtbnb_reviews/query`,
-          {
-            headers,
-            method: "POST",
-            body: JSON.stringify({
-              filters: `homeId:${homeId}`,
-              hitsPerPage: 6,
-              attributesToHighlight: [],
-            }),
-          }
-        )
-      );
-    } catch (error) {
-      return getErrorResponse(error);
-    }
-  }
+	async function getReviewsByHomeId(homeId) {
+		try {
+			return unWrap(
+				await fetch(
+					`https://${$config.algolia.APPLICATION_ID}-dsn.algolia.net/1/indexes/nuxtbnb_reviews/query`,
+					{
+						headers,
+						method: 'POST',
+						body: JSON.stringify({
+							filters: `homeId:${homeId}`,
+							hitsPerPage: 6,
+							attributesToHighlight: [],
+						}),
+					}
+				)
+			)
+		} catch (error) {
+			return getErrorResponse(error)
+		}
+	}
 
-  async function getUserByHomeId(homeId) {
-    try {
-      return unWrap(
-        await fetch(
-          `https://${$config.algolia.APPLICATION_ID}-dsn.algolia.net/1/indexes/nuxtbnb_users/query`,
-          {
-            headers,
-            method: "POST",
-            body: JSON.stringify({
-              filters: `homeId:${homeId}`,
-              attributesToHighlight: [],
-            }),
-          }
-        )
-      );
-    } catch (error) {
-      return getErrorResponse(error);
-    }
-  }
+	async function getUserByHomeId(homeId) {
+		try {
+			return unWrap(
+				await fetch(
+					`https://${$config.algolia.APPLICATION_ID}-dsn.algolia.net/1/indexes/nuxtbnb_users/query`,
+					{
+						headers,
+						method: 'POST',
+						body: JSON.stringify({
+							filters: `homeId:${homeId}`,
+							attributesToHighlight: [],
+						}),
+					}
+				)
+			)
+		} catch (error) {
+			return getErrorResponse(error)
+		}
+	}
 
-  async function getHomesByLocation(lat, lng, radiusInMeters = 1500 * 15) {
-    try {
-      return unWrap(
-        await fetch(
-          `https://${$config.algolia.APPLICATION_ID}-dsn.algolia.net/1/indexes/nuxtbnb_homes/query`,
-          {
-            headers,
-            method: "POST",
-            body: JSON.stringify({
-              aroundLatLng: `${lat},${lng}`,
-              aroundRadius: radiusInMeters,
-              hitsPerPage: 10,
-              attributesToHighlight: [],
-            }),
-          }
-        )
-      );
-    } catch (error) {
-      return getErrorResponse(error);
-    }
-  }
+	async function getHomesByLocation(
+		lat,
+		lng,
+		start,
+		end,
+		radiusInMeters = 1500 * 15
+	) {
+		try {
+			const days = []
+			for (var day = start; day <= end; day += 86400) {
+				days.push(`availability:${day}`)
+			}
+			return unWrap(
+				await fetch(
+					`https://${$config.algolia.APPLICATION_ID}-dsn.algolia.net/1/indexes/nuxtbnb_homes/query`,
+					{
+						headers,
+						method: 'POST',
+						body: JSON.stringify({
+							aroundLatLng: `${lat},${lng}`,
+							aroundRadius: radiusInMeters,
+							filters: days.join(' AND '),
+							hitsPerPage: 10,
+							attributesToHighlight: [],
+						}),
+					}
+				)
+			)
+		} catch (error) {
+			return getErrorResponse(error)
+		}
+	}
 
-  async function getHomes() {
-    try {
-      return unWrap(
-        await fetch(
-          `https://${$config.algolia.APPLICATION_ID}-dsn.algolia.net/1/indexes/nuxtbnb_homes/query`,
-          {
-            headers,
-            method: "POST",
-            body: JSON.stringify({
-              hitsPerPage: 3,
-              attributesToHighlight: [],
-            }),
-          }
-        )
-      );
-    } catch (error) {
-      return getErrorResponse(error);
-    }
-  }
+	async function getHomes() {
+		try {
+			return unWrap(
+				await fetch(
+					`https://${$config.algolia.APPLICATION_ID}-dsn.algolia.net/1/indexes/nuxtbnb_homes/query`,
+					{
+						headers,
+						method: 'POST',
+						body: JSON.stringify({
+							hitsPerPage: 3,
+							attributesToHighlight: [],
+						}),
+					}
+				)
+			)
+		} catch (error) {
+			return getErrorResponse(error)
+		}
+	}
 }
- 
